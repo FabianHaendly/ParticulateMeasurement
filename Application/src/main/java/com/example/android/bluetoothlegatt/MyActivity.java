86d7acfea0ecaee80c6023f67c10ff320e41615b
fabian77 <fabian77@web.de>
2019-03-04 20:47:30 +0100

    Implements turns on turn off BT

--- Application/src/main/java/com/example/android/bluetoothlegatt/MyActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/MyActivity.java
@@ -1,46 +1,129 @@
 package com.example.android.bluetoothlegatt;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MyActivity extends Activity {
+    private static String TAG = "New My Code";
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter mBluetoothAdapter;
+    Button mOnOffBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_view);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        View onOffLayout = findViewById(R.id.switch_btn);
+        mOnOffBtn = (Button) onOffLayout.findViewById(R.id.onOff_btn);
+        if (mBluetoothAdapter.isEnabled())
+            mOnOffBtn.setText("ON");
+        else
+            mOnOffBtn.setText("OFF");
+
+        mOnOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: ON/OFF btn clicked");
+                enableBT();
+            }
+        });
+    }
+
+    private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
+
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "onReceive: STATE OFF");
+                        mOnOffBtn.setText("TURN ON");
+                        Toast.makeText(getBaseContext(), "Bluetooth turned off", Toast.LENGTH_SHORT).show();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "onReceive: STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "onReceive: STATE ON");
+                        Toast.makeText(getBaseContext(), "Bluetooth turned on", Toast.LENGTH_SHORT).show();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "onReceive: STATE TURNING ON");
+                        break;
+                }
+            }
+        }
+    };
+
+    public void enableBT() {
+        if (mBluetoothAdapter == null) {
+            Log.d(TAG, "enableDisableBT: Does not have BT capabilities");
+        }
+        if (!mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "enableBT: enabling BT");
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBTIntent, REQUEST_ENABLE_BT);
+
+//          Intercepts changes in BT status + Log / ACTION_REQUEST_ENABLE is the state change
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+//          BroadcastReceiver catches the state change aka ACTION_STATE_CHANGED
+            registerReceiver(mBroadcastReceiver1, BTIntent);
+        }
+        if (mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "enableBT: disabling BT");
+            mBluetoothAdapter.disable();
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, BTIntent);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        Log.d(TAG, "onActivityResult: CATCHES BT ON OR OFF FROM USER DECISION");
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(getBaseContext(), "No measurement possible", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: called");
+        super.onDestroy();
+        try {
+            unregisterReceiver(mBroadcastReceiver1);
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, "onDestroy: Receiver 1 was not registered - " + e.getMessage());
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
 
-        MenuItem togBtn = menu.findItem(R.id.menu_bt_switch);
-        togBtn.setActionView(R.layout.switch_layout);
-
-        final Switch sw = (Switch) menu.findItem(R.id.menu_bt_switch).getActionView().findViewById(R.id.togBtn);
-
-        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked)
-                    Toast.makeText(getBaseContext(), "toggling", Toast.LENGTH_SHORT).show();
-                else{
-                    Toast.makeText(getBaseContext(), "not toggling", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
         menu.findItem(R.id.menu_stop).setVisible(false);
         menu.findItem(R.id.menu_refresh).setVisible(false);
         menu.findItem(R.id.menu_scan).setVisible(false);
-        menu.findItem(R.id.menu_bt_switch).setVisible(true);
-
-
 
         return true;
     }
